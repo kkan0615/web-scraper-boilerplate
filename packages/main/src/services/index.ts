601889d538f53c772cc13813ea1214a5209cbda2
kkan0615 <kkan0615@gmail.com>
2022-11-15 16:23:29 -0500

    :zap: Feat: add sample excel out

--- packages/main/src/services/index.ts
+++ packages/main/src/services/index.ts
@@ -7,10 +7,23 @@ ipcMain.handle('scraping', scraping)
 ipcMain.handle('test', async () => {
   console.log('testing...')
 
-  await exportToExcel('awesome-test.xlsx', [{
-    a: 'a',
-    b: 'b'
-  }])
+  await exportToExcel({
+    fileName: 'awesome-test',
+    fileExt: 'csv',
+    sheets: [
+      {
+        name: 'Test 1',
+        columns: [
+          { header: 'key', key: 'key' },
+          { header: 'value', key: 'value' },
+        ],
+        data: [
+          { key: 'a', value: '에이' },
+          { key: 'b', value: '비' }
+        ]
+      },
+    ]
+  })
 
   return true
 })
--- packages/main/src/utils/excel.ts
+++ packages/main/src/utils/excel.ts
@@ -2,32 +2,37 @@
 import execljs from 'ExcelJS'
 import { app } from 'electron'
 
-interface ExcelArgSheet<T> {
+interface ExcelArgSheet {
   name: string
-  data: T[]
+  columns?: any[]
+  data: any[]
 }
 
-interface ExcelArg<T> {
-  sheet: Record<string, ExcelArgSheet<T>>
+interface ExcelArg {
+  fileName: string
+  fileExt: 'xlsx' | 'csv'
+  sheets: ExcelArgSheet[]
 }
 
-export const exportToExcel = async (fileName: string, data: any[]) => {
+export const exportToExcel = async (arg: ExcelArg) => {
   // Create workbook
   const workbook = new execljs.Workbook()
   workbook.created = new Date()
-  // Create sheet
-  const sheet = workbook.addWorksheet('test sheet')
-  sheet.columns = [
-    { header: '에이', key: 'a' },
-    { header: '비', key: 'b' },
-  ]
-  // Add data
-  for (let i = 0; i < data.length; i++) {
-    console.log(data[i])
-    sheet.insertRow(1, data[i])
-  }
-  // const cell = sheet.getCell('C3')
-  // cell.value = new Date(1968, 5, 1)
+  arg.sheets.map((sheetEl: ExcelArgSheet) => {
+    // Create sheet
+    const sheet = workbook.addWorksheet(sheetEl.name)
+    // Add columns
+    if (sheetEl.columns) {
+      sheet.columns = sheetEl.columns
+    }
+    sheet.addRows(sheetEl.data)
+  })
   // Output xlsx type
-  await workbook.xlsx.writeFile(`${app.getPath('downloads')}/${fileName}`)
+  const filePath = `${app.getPath('downloads')}/${arg.fileName}.${arg.fileExt}`
+  if(arg.fileExt === 'xlsx') {
+    await workbook.xlsx.writeFile(filePath)
+  } else if (arg.fileExt === 'csv') {
+    await workbook.csv.writeFile(filePath)
+  }
+
 }
